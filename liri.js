c66b0c78187b6b8ae0ca543c1d3f58bca39c509d
arinmsn <arinmsn@gmail.com>
2019-02-25 17:33:39 -0800

    Project is completed :q exit clea clear

--- liri.js
+++ liri.js
@@ -23,22 +23,20 @@ var bit = keys.bit;
 var command = process.argv[2];
 // var userInput = process.argv[3];   //   .slice(3).join(" ")
 var userInput = process.argv.slice(3).join("+");
+str = userInput.split("+").join(" ")
+
+//  ^---- If we omit this line, user will see "artist+name" vs. "artist name"
+
+/*  **********
+var command = process.argv[2];
+// var userInput = process.argv[3];   //   .slice(3).join(" ")
+var userInput = process.argv.slice(3).join("+");
 str = userInput.split("+").join(" ")            
 //  ^---- If we omit this line, user will see "artist+name" vs. "artist name"
+                                                                                                */ 
 
 function welcome() {
 
-        // Welcome page
-
-    // console.log("   ┌──────────────────────────────────────────────────────┐ ");
-    // console.log("   │                         Welcome!                     │ ");
-    // console.log("   ╞══════════════════════════════════════════════════════╡ ");
-    // console.log("   |             LIRI is similiar to iPhone's SIRI.       │ ");
-    // console.log("   │        It's a command-line node app that will        │ ");
-    // console.log("   │   return data back to you. Follow the instructions   │ ");
-    // console.log("   │                 and start searching.                 │ ");
-    // console.log("   └──────────────────────────────────────────────────────┘ ");
-    // console.log("");      
     console.log("   ┌─────────────────────────────────────────────────────────────────────────────────────────┐ ");
     console.log("   │                                         Welcome!                                        │ ");
     console.log("   ╞═════════════════════════════════════════════════════════════════════════════════════════╡ ");
@@ -47,8 +45,23 @@ function welcome() {
     console.log("   │       Recognition Interface. LIRI will be a command line node app that takes in         │ ");
     console.log("   │                            parameters and gives you back data.                          │ ");
     console.log("   │                                                                                         │ ");
+    console.log("   ├─────────────────────────────────────────────────────────────────────────────────────────┤ ");
+    console.log("   │                                         Instructions                                    │ ");
+    console.log("   ╞═════════════════════════════════════════════════════════════════════════════════════════╡ ");
+    console.log("   │                                                                                         │ ");
+    console.log("   |  No need to use ' ' and < >                                                             │ ");
+    console.log("   │  To get concert information:  node liri.js 'concert-this` <'artist/band name'>          │ ");
+    console.log("   │  To get info. about a song :  node liri.js 'spotify-this-song` <'song' 'artist/band'>   │ ");
+    console.log("   │  To get a movie's info:       node liri.js 'movie-this` <'movie title'>                 │ ");
+    console.log("   │  To let app randomly run:     node liri.js 'do-what-it-says'                            │ ");
+    console.log("   │                                                                                         │ ");
     console.log("   └─────────────────────────────────────────────────────────────────────────────────────────┘ ");
-    
+    if (command === "" || command === undefined) {
+        command = "User entered empty or undefined command."
+        logData(`\n\nCommand: ${command}`);
+    } else{ 
+        logData(`\n\nCommand: ${command}`);
+    }
 } 
 
 // init(command, userInput);
@@ -72,15 +85,10 @@ function init(command, userInput) {
         case "do-what-it-says":
             doIt(userInput);
             break;
-        
-        // case "help":
-        //     welcome();
-        //     helpUser();
-        //     break;
 
         default:
+        case "help":
             welcome();
-            helpUser();
             break;
     } // End of swtich
 }
@@ -89,36 +97,41 @@ init(command, userInput);
 function concertIt(userInput) {
     if (!userInput) {
         userInput = "Yanni";
+        str = "Yanni";
     } 
     var queryURL = `https://rest.bandsintown.com/artists/${userInput}/events?app_id=`+bit;
     request(queryURL, function(error, response, body) {
         if (error) {
             return console.log("Error (Concert-this): " + error);
         }
-        
+
         if (!error && response.statusCode === 200) {
             var data = JSON.parse(body);
             if (data.length > 0) {
+            
                 for (i=0; i<1; i++){
-                    console.log("   Band/artist       : " + userInput) 
+                    console.log("   Band/artist       : " + str); // userInput.split("+").join(" ")
+                    var venueName = data[i].venue.name;
+                    var venueLocation = data[i].venue.city;
                     console.log("   Name of venue     : " + data[i].venue.name);
                     console.log("   Venue location    : " + data[i].venue.city);
-                    var date = moment(date).format("MM/DD/YYYY");
+                    var date = moment(data[i].datetime).format("MM/DD/YYYY");
                     console.log("   Date of the Event : " + date); 
                 };
+
+                // Output data for logging 'log.txt'
+                var outputData = `\n  ----- Concert Info. -----\nCommand: node liri.js ${command}\nSearched for: ${str}\n` +
+                `Name of venue: ${venueName}\nVenue Location: ${venueLocation}\nDate of the event: ${date}\n`
+                logData(outputData);
             }
         }
     });
-};
+};      // End of concertIt()
 
 function spotifyIt(userInput) {
     
     if (!userInput) {
-        // return console.log("You didn't enter a song name!");
-        userInput = "I Want it That way"; 
-        //If we omit this and random.txt
-        //is empty, we will get error
-        // doIt();
+        userInput = "the sign, the ace of base"; 
     }
 
     spotify
@@ -134,13 +147,23 @@ function spotifyIt(userInput) {
         
         var song = data.tracks.items[0];
         console.log("");
+        var artistName = song.album.artists[0].name;
+        var songName   = song.name;
+        var preview    = song.external_urls.spotify;
+        var album      = song.album.name;
         console.log("    Artist(s) :  " + song.album.artists[0].name);
         console.log("    Song :  " + song.name);
         console.log("    Preview link of song from Spotify :  " + song.external_urls.spotify); 
         console.log("    Album the song is from :  " + song.album.name);
+
+        // Output data for logging 'log.txt'
+        var outputData = `\n  ----- Song Info. -----\nCommand: node liri.js ${command}\nSearched for: ${userInput}\n` +
+        `Artist: ${artistName}\nSong: ${songName}\nPreview link of song from Spotify: ${preview}\n\n`
     })
 
-}
+    
+
+}       // End of spotifyIt()
 
 function movieIt() {
     // if (userInput == "" || !value){
@@ -155,22 +178,34 @@ function movieIt() {
     
     axios.get("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&page=3&apikey=trilogy")
         .then(function (response) { 
-            console.log("   ──────────────────────────────────────────────────────");
-            console.log("   Title of the movie: " + response.data.Title);
-            console.log("   Year the movie came out: " + response.data.Year);
-            console.log("   IMDB Rating of the movie: " + response.data.imdbRating);
-            console.log("   Country where the movie was produced: " + response.data.Country);
-            console.log("   Language of the movie: " + response.data.Language);
-            console.log("   Plot of the movie: " + response.data.Plot);
-            console.log("   Actors in the movie: " + response.data.Actors);
+        console.log("   ──────────────────────────────────────────────────────");
+        console.log("   Title of the movie: " + response.data.Title);
+        console.log("   Year the movie came out: " + response.data.Year);
+        console.log("   IMDB Rating of the movie: " + response.data.imdbRating);
+        console.log("   Country where the movie was produced: " + response.data.Country);
+        console.log("   Language of the movie: " + response.data.Language);
+        console.log("   Plot of the movie: " + response.data.Plot);
+        console.log("   Actors in the movie: " + response.data.Actors);
             // Format the short play (split @ ".") and join "./n"
         //   console.log(response);
+        // Output data for logging 'log.txt'
+        var movieName = response.data.Title;
+        var movieData = `\nTitle of the movie: ${movieName}`+
+        `\nYear the movie came out: ${response.data.Year}` +
+        `\nIMDB Rating of the movie: ${response.data.imdbRating}` +
+        `\nCountry where the movie was produced: ${response.data.Country}`+
+        `\nLanguage of the movie: ${response.data.Language}`+
+        `\nPlot of the movie: ${response.data.Plot}`+
+        `\nActors in the movie: ${response.data.Actors}`
+
+        var outputData = `\n\n  ----- Movie Info. -----\nCommand: node liri.js ${command}\nSearched for: ${movieName}\n` + movieData
+        logData(outputData);
         })
         .catch(function (error) {
           // handle error
           console.log(error);
         })
-}
+}       // End of movieIt()
 
 function doIt() {
     fs.readFile("random.txt", "utf8", function(err, data) {
@@ -197,23 +232,16 @@ function doIt() {
                 movieIt(userInput);
                 break;
         }
+        // Output data for logging 'log.txt'
+        var outputData = `\n  ----- Do What It Says -----\nCommand: node liri.js ${command}\n`
+        logData(outputData);
     })
 } // End of doIt()
 
-function helpUser() {
-    console.log("   ┌─────────────────────────────────────────────────────────────────────────────────────────┐ ");
-    console.log("   │                                         Instructions                                    │ ");
-    console.log("   ╞═════════════════════════════════════════════════════════════════════════════════════════╡ ");
-    console.log("   │                                                                                         │ ");
-    console.log("   |  No need to use ' ' and < >                                                             │ ");
-    console.log("   │  To get concert information:  node liri.js 'concert-this` <'artist/band name'>          │ ");
-    console.log("   │  To get info. about a song :  node liri.js 'spotify-this-song` <'song', 'artist/band'>  │ ");
-    console.log("   │  To get a movie's info:       node liri.js 'movie-this` <'movie title'>                 │ ");
-    console.log("   │  To let app randomly run:     node liri.js 'do-what-it-says'                            │ ");
-    console.log("   │                                                                                         │ ");
-    console.log("   │                                                                                         │ ");
-    console.log("   └─────────────────────────────────────────────────────────────────────────────────────────┘ ");
-    console.log("");
-}
-
-//If you need help:            node liri.js 'help' 
+function logData(outputData) {
+    fs.appendFile("log.txt", outputData, err => {
+        if (err) throw err;
+        
+        console.log("---Log is saved!");
+    })
+} // End of logData()
